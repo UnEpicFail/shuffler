b4504134f999986b39581c33ae0ba5cc002e8f8a
Konstantin Kulik <k.kulik@netrika.ru>
2016-06-01 15:52:18 +0300

    fix Warnings

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.use('/static', express.static(__dirname + '/public'));
-app.use('/libs', express.static(__dirname + '/node_modules'));
+app.use('/static', express.static(__dirname + '/public'))
+app.use('/libs', express.static(__dirname + '/node_modules'))
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname +'/public/views/index.html')
-});
-
-app.post('/api/signin', function (req, res){
-  console.log('req.params', req.params);
-  res.send('');
+  res.sendFile(__dirname + '/public/views/index.html')
 })
 
-app.post('/api/signup', function (req, res){
-  console.log('req.params', req.params);
-  res.send('');
+app.post('/api/signin', function (req, res) {
+  console.log('req.params', req.params)
+  res.send('')
+})
+
+app.post('/api/signup', function (req, res) {
+  console.log('req.params', req.params)
+  res.send('')
 })
 
 app.listen(3000, function () {
-  console.log('I\'m start');
-});
+  console.log('I\'m start')
+})
